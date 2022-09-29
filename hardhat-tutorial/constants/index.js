3f3fd38a94ff47493d152fe701134ff792c75304
Lumaqua <8faizahmed@gmail.com>
2022-09-28 21:26:32 -0400

    Adding first version of project files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1E0053e31e5AF2FC2aC61EbD9f281A9d0f01BCb6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
